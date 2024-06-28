1a88dd768e8fa8744b4b6bd2dbd7f268f68f1e0f
Samuell Torres <samuelltorres145@gmail.com>
2024-06-28 15:31:01 -0300

    feat: Impedindo a criação de hábitos duplicados

--- src/controllers/habits.controller.ts
+++ src/controllers/habits.controller.ts
@@ -5,6 +5,14 @@ export class HabitsController {
 	store = async (request: Request, response: Response): Promise<Response> => {
 		const { name } = request.body;
 
+		const findHabit = await habitModel.findOne({
+			name,
+		});
+
+		if (findHabit) {
+			return response.status(400).json({ message: 'Habit already exists.' });
+		}
+
 		const newHabit = await habitModel.create({
 			name,
 			completedDates: [],
