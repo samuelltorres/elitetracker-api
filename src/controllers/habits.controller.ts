43d2f4ea7ee4c6590555d363a84392b370aeb275
Samuell Torres <samuelltorres145@gmail.com>
2024-06-28 15:59:09 -0300

    feat: Validação da requisição com o Zod

--- src/controllers/habits.controller.ts
+++ src/controllers/habits.controller.ts
@@ -1,12 +1,27 @@
 import type { Request, Response } from 'express';
+import { z } from 'zod';
 import { habitModel } from '../models/habit.model';
 
 export class HabitsController {
 	store = async (request: Request, response: Response): Promise<Response> => {
+		const schema = z.object({
+			name: z.string(),
+		});
+
 		const { name } = request.body;
 
-		const findHabit = await habitModel.findOne({
+		const habit = schema.safeParse({
 			name,
+		});
+
+		if (!habit.success) {
+			return response.status(400).json({
+				message: 'Error on validation.',
+			});
+		}
+
+		const findHabit = await habitModel.findOne({
+			name: habit.data.name,
 		});
 
 		if (findHabit) {
@@ -14,7 +29,7 @@ export class HabitsController {
 		}
 
 		const newHabit = await habitModel.create({
-			name,
+			name: habit.data.name,
 			completedDates: [],
 		});
 
